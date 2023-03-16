af807ac850e03ed3669b314e97e26675d4b4a475
ElvinRustamov <109756203+ElvinRustamov@users.noreply.github.com>
2023-03-16 15:10:20 +0400

    Create main.py

---
+++ DiceRoll/main.py
@@ -0,0 +1,10 @@
+class DiceRoll:
+    import tkinter
+    import random
+    from tkinter import messagebox
+    MIN = 1
+    MAX = 6
+
+    def __init__(self):
+        self.root = self.tkinter.Tk()
+        self.screen()
